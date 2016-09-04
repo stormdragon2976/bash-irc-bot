a190c08bdf9ebc49de4e1700abdb18e31a1e63f9
Storm Dragon <stormdragon2976@gmail.com>
2016-09-04 19:15:51 -0400

    Working link parser.

--- triggers/link/link.sh
+++ triggers/link/link.sh
@@ -1,8 +1,9 @@
 [ -f functions.sh ] && source functions.sh
 
+curl="$(command -v curl)"
 for l in $3 ; do
 if [[ "$l" =~ http://|https://|www\.* ]]; then
-pageTitle="$(curl -s "$l" | grep "<title>" | sed 's#\(</\|</\)title>##g')"
+pageTitle="$($curl -Ls "${l/#:/}" | grep "<title>" | html2text | tr '[:space:]' ' ')"
 shortLink="${l#*://}"
 shortLink="${shortLink%%/*}"
 msg "$2" "$pageTitle at $shortLink"
