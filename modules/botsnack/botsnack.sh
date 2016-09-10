4b98d1e4df8532df338c938b18d4b71ffc4095ad
Storm Dragon <stormdragon2976@gmail.com>
2016-09-10 10:41:13 -0400

    Reward the bot with the botsnack module.

---
+++ modules/botsnack/botsnack.sh
@@ -0,0 +1,14 @@
+[ -f functions.sh ] && source functions.sh
+
+userNick="$1"
+shift
+chan="$1"
+shift
+snack="${@:-$(shuf -n1 -e\
+  "BBQ microchips"\
+  "BBQ corn nuts"\
+  "steak")}"
+thanks="$(shuf -n1 -e "Thank you" "You're so awesome" "You shouldn't have" "You rock")"
+favorite="$(shuf -n1 -e "my favorite" "yum yum" "this is bot heaven" "DELICIOUS")"
+
+echo "$chan" "$thanks $userNick: $snack! $favorite!"
