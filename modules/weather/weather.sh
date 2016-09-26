00adbf98e9543831e862368d227c6b2fc79554c2
Storm Dragon <stormdragon2976@gmail.com>
2016-09-26 15:29:17 -0400

    minor updates.

--- modules/weather/weather.sh
+++ modules/weather/weather.sh
@@ -4,6 +4,7 @@ if [ ${#3} -ge 5 ]; then
 weatherInfo="$(curl -s "http://mobile.wunderground.com/cgi-bin/findweather/getForecast?brand=mobile&query=$3")"
 weatherTemperature="$(echo "$weatherInfo" | grep -A 2 '<tr><td>Temperature</td>' | tr -cd '[:digit:]-.')"
 weatherConditions="$(echo "$weatherInfo" | grep -A 1 '<tr><td>Conditions</td>' | tail -n1 | sed 's/<[^>]*>//g')"
+weatherConditions="${weatherConditions/Rain/wetter than a teenage girl at a boy band concert}"
 weatherInfo="Currently your weather is $weatherConditions and $weatherTemperature degrees fahrenheit."
 fi
 
