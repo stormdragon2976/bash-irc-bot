ba62a946b12a7f0ca1341ec03d7aed4b7a07e9f9
Storm Dragon <stormdragon2976@gmail.com>
2017-04-03 17:59:44 -0400

    Fixed small error with rain in weather functions.

--- modules/weather/weather.sh
+++ modules/weather/weather.sh
@@ -1,10 +1,11 @@
 [ -f functions.sh ] && source functions.sh
 
+set -f
 if [ ${#3} -ge 5 ]; then
 weatherInfo="$(curl -s "http://mobile.wunderground.com/cgi-bin/findweather/getForecast?brand=mobile&query=$3")"
 weatherTemperature="$(echo "$weatherInfo" | grep -A 2 '<tr><td>Temperature</td>' | tr -cd '[:digit:]-.')"
 weatherConditions="$(echo "$weatherInfo" | grep -A 1 '<tr><td>Conditions</td>' | tail -n1 | sed 's/<[^>]*>//g')"
-weatherConditions="${weatherConditions/Rain/wetter than a teenage girl at a boy band concert}"
+weatherConditions="${weatherConditions/*Rain/wetter than a teenage girl at a boy band concert}"
 weatherInfo="Currently your weather is $weatherConditions and $weatherTemperature degrees fahrenheit."
 fi
 
@@ -12,3 +13,4 @@ if [ ${#weatherInfo} -lt 5 ]; then
 weatherInfo="Sorry $1, no weather found for $3."
 fi
 msg "$2" "$1: $weatherInfo"
+set +f
