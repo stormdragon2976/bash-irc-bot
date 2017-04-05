c0ab59361463724579f88accd93f909b5cc75152
Storm Dragon <stormdragon2976@gmail.com>
2017-04-05 16:58:30 -0400

    Douchebag module added.

---
+++ modules/douchebag/douchebag.sh
@@ -0,0 +1,6 @@
+[ -f functions.sh ] && source functions.sh
+
+douchebag=(
+"Don't ask to ask, just ask!"
+'Use a pastebin website, bun only the one approved by the users of this channel, else someone may flood your screen with whining and bitching!')
+msg "$2" "$1: $(shuf -n1 -e "${advice[@]}")"
