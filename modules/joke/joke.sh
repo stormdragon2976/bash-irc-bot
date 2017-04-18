62b7f331ccf26c312b0a4b8575e70353773d87f8
Storm Dragon <stormdragon2976@gmail.com>
2017-04-18 19:46:10 -0400

    excluded little_johnny jokes because they are too long, and excluded sex because this bot already has that functionaltiy uas the sex function.

--- modules/joke/joke.sh
+++ modules/joke/joke.sh
@@ -1,5 +1,5 @@
 [ -f functions.sh ] && source functions.sh
 
-joke="$(curl -s 'https://thatsagooden.tk/?api=txt&type=all')"
+joke="$(curl -s 'https://thatsagooden.tk/?api=txt&type=all&exclude=sex,little_johnny')"
 joke="${joke//[[:space:]]/ }"
 msg "$2" "$joke"
