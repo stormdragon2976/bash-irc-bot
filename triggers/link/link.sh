96b4400f5e221fbd61f1e327f8c469648b0aaef4
Storm Dragon <stormdragon2976@gmail.com>
2017-05-20 08:33:05 -0400

    Added link timeout ffor titles.

--- triggers/link/link.sh
+++ triggers/link/link.sh
@@ -1,6 +1,6 @@
 [ -f functions.sh ] && source functions.sh
 
-curl="$(command -v curl)"
+curl="$(command -v curl) --connect-timeout 5"
 for l in $3 ; do
 if [[ "${l#:}" =~ http://|https://|www\.*\.* ]]; then
 pageTitle="$($curl -Ls "${l#:}" | grep "<title>" | html2text | tr -s '[:space:]' ' ')"
