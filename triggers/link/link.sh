a0553d8378e04351ca644fcc5b2a6e9b879bf14d
Storm Dragon <stormdragon2976@gmail.com>
2017-09-11 09:12:15 -0400

    Fixed some weirdness in the link title trigger.

--- triggers/link/link.sh
+++ triggers/link/link.sh
@@ -3,7 +3,7 @@
 curl="$(command -v curl) --connect-timeout 5"
 for l in $3 ; do
 if [[ "${l#:}" =~ http://|https://|www\.*\.* ]]; then
-pageTitle="$($curl -Ls "${l#:}" | grep "<title>" | html2text | tr -s '[:space:]' ' ')"
+pageTitle="$($curl -Ls "${l#:}" | sed -n 's/<title>\(.*\)<\/title>/\1/p' | head -1 | html2text | tr -s '[:space:]' ' ')"
 shortLink="${l#*://}"
 shortLink="${shortLink%%/*}"
 if [ ${#pageTitle} -gt 1 ]; then
