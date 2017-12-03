a059aa6bb7e8d274224cc73ece91d3c2c0ca4697
Storm Dragon <stormdragon2976@gmail.com>
2017-12-02 20:47:28 -0500

    Fixed a bug in the new sing module.

--- modules/sing/sing.sh
+++ modules/sing/sing.sh
@@ -4,8 +4,9 @@ shift
 chan="$1"
 shift
 #get the lyric text into a variable
-lyricText="$(clyrics $@ | tr '[:space:]' ' ' | tr -s ' ' | fold -s -w 160)"
+lyricText="$(clyrics $@ | tr '[:space:]' ' ' | tr -s ' ' | fold -s -w 256)"
 i=$(echo "$lyricText" | wc -l)
+i=$(($RANDOM % $i + 1))
 lyricText="$(echo "$lyricText" | tail +$i | head -1)"
 #Display the lyric text
 if [ ${#lyricText} -gt 15 ] ; then
