511faf83f81946c3030813478317df9871a651af
Storm Dragon <stormdragon2976@gmail.com>
2017-12-11 09:40:52 -0500

    upgraded hands free douchebag functionality.

--- modules/douchebag/douchebag.sh
+++ modules/douchebag/douchebag.sh
@@ -2,6 +2,7 @@
 
 douchebag=(
 "Don't ask to ask, just ask!"
+"STOP! READ THIS BEFORE YOU SPEAK! http://www.rockbox.org/wiki/IrcGuidelines"
 "The human requesting this service can't be bothered to help you in person, so they requested a bot tell you that we don't discuss blah here, only blah+ which is totally different."
 'Use a pastebin website, bun only the one approved by the users of this channel, else someone may flood your screen with whining and bitching!')
 msg "$2" "$1: $(shuf -n1 -e "${douchebag[@]}")"
